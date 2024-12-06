f4ca356a7c7b550603fe4c197b5ff0085d793f1f
ARSH AMIRALI <arshamirali37>
2024-12-06 12:04:29 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,59 @@
+//                                     {Home work}
+
+const arrays = [42, true, "Hello, World!", { name: "John", age: 25, isStudent: false, }, false, 3.14, "JavaScript is fun!",];
+
+// Q1: Convert all elements to their string representation using map Method?
+//     let string = arrays.map((e, i) => {
+//         return "Arsh"
+//     })
+// console.log(string);
+
+
+// Q2: Filter only number elements using filter Method?
+//     console.log(numbersOnly)
+// let newarr = arrays.filter((e, i) => {
+//     return typeof e === "number";
+// })
+// console.log(newarr)
+
+
+// Q3: Log each element's type to the console (does not return anything) using forEach Method?
+// let Arsh = arrays.forEach((e, i) => {
+//     console.log(typeof e);
+// })
+// console.log(Arsh);
+
+
+// Q4: Sum up only the numbers in the array reduce Method ?
+//     let element = arrays.reduce((a, b) => {
+//   return typeof number == a + b
+//     })
+// console.log(element);
+
+
+// Q5: Find the first boolean value in an array using Find Method?
+// let FIND = arrays.find((E,I)=>{
+// return typeof E === "boolean"
+// })
+// console.log(FIND);
+
+
+// Q6: Find the index of the first object in an array using FindIndex Method?
+// let findIndex = arrays.findIndex((E,I)=>{
+// return typeof E === "object"
+// })
+// console.log(findIndex);
+
+
+// Q7: Check if the array contains any numbers in an array using some method?
+// let hasNumbers = arrays.some((element, index) => {
+//     typeof element === "number"
+// });
+// console.log(hasNumbers)
+
+
+// Q8: Check if all elements are of type 'string' in an array using every method?
+// let Arr=arrays.every((e,i) =>{
+// return typeof e == "string"
+// })
+// console.log(Arr)
